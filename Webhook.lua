5afe804bd203186d7d8c019bd7937cbdb9581cea
HellWayer <110084751+HellWayer@users.noreply.github.com>
2023-01-01 20:32:26 +0700

    Create Webhook.lua

---
+++ Webhook.lua
@@ -0,0 +1,32 @@
+--[[
+
+    ██╗░░░░░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░  ██╗░░██╗██╗░░░██╗██████╗░
+    ██║░░░░░██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗  ██║░░██║██║░░░██║██╔══██╗
+    ██║░░░░░██║░░██║██║░░╚═╝█████═╝░█████╗░░██████╔╝  ███████║██║░░░██║██████╦╝
+    ██║░░░░░██║░░██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗  ██╔══██║██║░░░██║██╔══██╗
+    ███████╗╚█████╔╝╚█████╔╝██║░╚██╗███████╗██║░░██║  ██║░░██║╚██████╔╝██████╦╝
+    ╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝░╚═════╝░╚═════╝░
+
+]]
+
+local url = "https://discord.com/api/webhooks/" .. Id .. "/" .. token
+local data = {
+    ["content"] = "",
+    ["embeds"] = {
+        {
+            ["title"] = "API SERVICES LUA",
+            ["description"] = "\nDisplayName : ".. game.Players.LocalPlayer.DisplayName .."\n\nUser : " .. game.Players.LocalPlayer.Name .."\n\nUserID : ".. game.Players.LocalPlayer.UserId .."\n\nRobloxVersion : ".. game:GetService("RunService"):GetRobloxVersion() .."\n\nKey : " ..getgenv().Key .. "\n\nHWID : " .. game:GetService("RbxAnalyticsService"):GetClientId() ..,
+            ["type"] = "rich",
+        }
+    }
+}
+local data = game:GetService("HttpService"):JSONEncode(data)
+local header = {["content-type"] = "application/json"}
+local request = http_request or request or HttpPost or syn.request
+local serversdata = {
+    Url = url, 
+    Body = data, 
+    Method = "POST", 
+    Headers = header
+}
+request(serversdata)
