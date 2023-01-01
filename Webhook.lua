8b0a45e20e7622afd665c38fd166aad1e7d8af8c
HellWayer <110084751+HellWayer@users.noreply.github.com>
2023-01-02 01:43:10 +0700

    Update Webhook.lua

--- Webhook.lua
+++ Webhook.lua
@@ -1,3 +1,4 @@
+
 --[[
 
     ██╗░░░░░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░  ██╗░░██╗██╗░░░██╗██████╗░
@@ -9,28 +10,33 @@
 
 ]]
 
-task.wait()
+if getgenv().Discordid == "" or getgenv().Discordid == nil then
+    getgenv().Discordid = "-"
+end
+if getgenv().Key == "" or getgenv().Key == nil then
+    getgenv().Key = "-" 
+end
 
-local url = "https://discord.com/api/webhooks/" .. Id .. "/" .. token
-local data = {
-    ["content"] = "",
-    ["embeds"] = {
-        {
-            ["title"] = "API SERVICES LUA",
-            ["description"] = "\nDisplayName : ".. game.Players.LocalPlayer.DisplayName .."\n\nUser : " .. game.Players.LocalPlayer.Name .."\n\nUserID : ".. game.Players.LocalPlayer.UserId .."\n\nRobloxVersion : ".. game:GetService("RunService"):GetRobloxVersion() .."\n\nKey : " ..getgenv().Key .. "\n\nHWID : " .. game:GetService("RbxAnalyticsService"):GetClientId() ..,
-            ["type"] = "rich",
+local a = {}
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Whilelist-Checker/main/Webhook.lua", true))()
+    local url = "https://discord.com/api/webhooks/" .. Id .. "/" .. token
+    local data = {
+        ["content"] = "",
+        ["embeds"] = {
+            {
+                ["title"] = "API SERVICES LUA",
+                ["description"] = "\n----------------------------List Primary------------------------------\n\n\nDisplayName : ".. game.Players.LocalPlayer.DisplayName .. "\n\nUser : " .. game.Players.LocalPlayer.Name .. "\n\nUserID : " .. game.Players.LocalPlayer.UserId .. "\n\nRobloxVersion : ".. game:GetService("RunService"):GetRobloxVersion() .. "\n\n----------------------------List Advanced------------------------------\n\n\n\nDiscordID : " .. getgenv().Discordid .. "\n\nKey : " .. getgenv().Key .. "\n\nHardware ID : " .. game:GetService("RbxAnalyticsService"):GetClientId() .."\n",
+                ["type"] = "rich",
+            }
         }
     }
-}
-local data = game:GetService("HttpService"):JSONEncode(data)
-local header = {["content-type"] = "application/json"}
-local request = http_request or request or HttpPost or syn.request
-local serversdata = {
+    local data = game:GetService("HttpService"):JSONEncode(data)
+    local header = {["content-type"] = "application/json"}
+    local request = http_request or request or HttpPost or syn.request
+    local serversdata = {
     Url = url, 
     Body = data, 
     Method = "POST", 
     Headers = header
 }
 request(serversdata)
-
-task.wait()
